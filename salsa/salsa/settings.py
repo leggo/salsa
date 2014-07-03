126ee40a5fcc5f087ca840136bfc4567a4d921c6
leggo <acorley@gmx.de>
2014-07-03 14:55:25 +0200

    created app
    now going to Aldi

--- salsa/salsa/settings.py
+++ salsa/salsa/settings.py
@@ -120,10 +120,13 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+	
+	'rango',
 )
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
