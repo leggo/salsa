d5850c9914156fecdd749c1a6e31897f91aaca87
leggo <acorley@gmx.de>
2014-07-04 00:37:01 +0200

    finished chapter 3
    two views and some urlpatterns

--- salsa/rango/views.py
+++ salsa/rango/views.py
@@ -1,1 +1,7 @@
-# Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+	return HttpResponse("Halli, Hallo, Haalle! Here is a link to <a href='http://127.0.0.1:8000/rango/about/'>about</a> ")
+	
+def about(request):
+	return HttpResponse('Yes, this is abuut. And <a href="http://127.0.0.1:8000/rango/">here</a> is a link to the home page')
