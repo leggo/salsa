055eab9bbe5bb5e9254d1e682d0d92428cdc92d5
leggo <acorley@gmx.de>
2014-07-07 16:51:05 +0200

    start with templates
    you heard me!!

--- salsa/rango/views.py
+++ salsa/rango/views.py
@@ -1,7 +1,15 @@
 from django.http import HttpResponse
+from django.template import RequestContext
+from django.shortcuts import render_to_response
 
 def index(request):
-	return HttpResponse("Halli, Hallo, Haalle! Here is a link to <a href='http://127.0.0.1:8000/rango/about/'>about</a> ")
+	context = RequestContext(request)
+	context_dict = { 'boldmessage': "I am bold font from the context"}
+	return render_to_response('rango/index.html', context_dict, context)
+	
+	
+	
+	#return HttpResponse("Halli, Hallo, Haalle! Here is a link to <a href='http://127.0.0.1:8000/rango/about/'>about</a> ")
 	
 def about(request):
 	return HttpResponse('Yes, this is abuut. And <a href="http://127.0.0.1:8000/rango/">here</a> is a link to the home page')
