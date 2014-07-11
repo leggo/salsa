582457c312003cc0e4c9ecff1c4a79318af06f72
leggo <acorley@gmx.de>
2014-07-11 14:33:46 +0200

    finished chapter 4
    static files and templates

--- salsa/rango/views.py
+++ salsa/rango/views.py
@@ -12,4 +12,7 @@ def index(request):
 	#return HttpResponse("Halli, Hallo, Haalle! Here is a link to <a href='http://127.0.0.1:8000/rango/about/'>about</a> ")
 	
 def about(request):
-	return HttpResponse('Yes, this is abuut. And <a href="http://127.0.0.1:8000/rango/">here</a> is a link to the home page')
+	context = RequestContext(request)
+	context_dict = { 'schlurp': "Hi, my name is Schlurp!"}
+	return render_to_response('rango/about.html', context_dict, context)
+	#return HttpResponse('Yes, this is abuut. And <a href="http://127.0.0.1:8000/rango/">here</a> is a link to the home page')
