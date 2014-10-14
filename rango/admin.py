ce9cb8e7d4fbb7ccdf12f5d9502599cb1d82de57
leggo <acorley@gmx.de>
2014-10-14 05:18:34 +0200

    login function

--- rango/admin.py
+++ rango/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from rango.models import Category, Page
+from rango.models import Category, Page, UserProfile
 
 
 
@@ -9,3 +9,4 @@ class PageAdmin(admin.ModelAdmin):
 
 admin.site.register(Category)
 admin.site.register(Page, PageAdmin)
+admin.site.register(UserProfile)
